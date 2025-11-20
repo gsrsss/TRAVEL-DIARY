957c8f29ecad8d95a76481b5ef7adc634004b111
gsrsss <gsrsotos@gmail.com>
2025-11-19 23:58:53 -0500

    Update diary_logic.py

--- diary_logic.py
+++ diary_logic.py
@@ -1,7 +1,9 @@
 import json
 import os
+import uuid # Para generar nombres únicos a las fotos
 
 DATA_PATH = "data/diary.json"
+IMAGES_DIR = "data/images" # Carpeta para las fotos
 
 def load_diary():
     if not os.path.exists(DATA_PATH):
@@ -13,17 +15,32 @@ def load_diary():
         except:
             return []
 
-def save_entry(date, location, text):
+def save_entry(date, location, text, image_file=None):
     diary = load_diary()
+    
     new_entry = {
+        "id": str(uuid.uuid4()),
         "date": date,
         "location": location,
-        "text": text
+        "text": text,
+        "image_path": None # Por defecto sin foto
     }
+
+    if image_file is not None:
+        # 1. Crear carpeta de imágenes si no existe
+        if not os.path.exists(IMAGES_DIR):
+            os.makedirs(IMAGES_DIR)
+
+        filename = f"{location}_{uuid.uuid4()}.png".replace(" ", "_")
+        file_path = os.path.join(IMAGES_DIR, filename)
+
+        image_file.save(file_path, "PNG")
+        
+        new_entry["image_path"] = file_path
+
     diary.append(new_entry)
 
     directory = os.path.dirname(DATA_PATH)
-    
     if directory and not os.path.exists(directory):
         os.makedirs(directory)
 
@@ -32,4 +49,3 @@ def save_entry(date, location, text):
 
 def get_entries():
     return load_diary()
-
